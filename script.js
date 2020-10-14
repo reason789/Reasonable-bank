35fc41e41eca07cc7dbf238bbc5e4fd016adeb15
reason789 <rahadul134@gmail.com>
2020-10-14 07:34:25 +0600

    complete Reasonale Bank

--- script.js
+++ script.js
@@ -12,7 +12,39 @@ btn.addEventListener('click', function(){
 // Deposit button event handler
 var depositBtn = document.getElementById('addDeposit');
 depositBtn.addEventListener('click', function(){
-    const depositAmount = document.getElementById('pickDeposit').value;
-    var amount = parseFloat(depositAmount);
-    console.log(amount);
+    // const depositAmount = document.getElementById('pickDeposit').value;
+    // var depositNumber = parseFloat(depositAmount);
+    const depositNumber= getInputNumber("pickDeposit");
+    
+    updateSpanText('currentDeposit',depositNumber);
+    updateSpanText('currentBalance',depositNumber);
+
+
+    document.getElementById('pickDeposit').value = "";
 })
+
+// Withdraw button event handler
+
+const withdrawBtn = document.getElementById('addWithdraw');
+withdrawBtn.addEventListener('click', function(){
+    const withdrawNumber = getInputNumber('withdrawAmount');
+    updateSpanText("currentWithdraw", withdrawNumber);
+    updateSpanText("currentBalance", -1*withdrawNumber);
+
+    document.getElementById('withdrawAmount').value = "";
+})
+
+function getInputNumber(id){
+    const withdrawAmount = document.getElementById(id).value;
+    const withdrawNumber = parseFloat(withdrawAmount);
+    return withdrawNumber;
+}
+
+function updateSpanText(id,depositNumber)
+{
+    const currentBalance = document.getElementById(id).innerText;
+    const currentBalanceNumber = parseFloat(currentBalance);
+    const totalBalance = currentBalanceNumber + depositNumber;
+
+    document.getElementById(id).innerText = totalBalance;
+}
